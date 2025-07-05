9b493e3ab3d62438121bdd6bb1db3d339b4ce37b
Ulrik Friis-Jensen <ulrik98@gmail.com>
2025-07-05 16:59:09 +0200

    Changing for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,8 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import sphinx_rtd_theme
-
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # -- Project information
 
 project = 'autoXAS'
@@ -20,9 +21,8 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
